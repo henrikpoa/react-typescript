5a2977b80a1165d729b855d86c52ceffaa3e8d43
henrikpoa <henrik_poa@hotmail.com>
2024-12-06 18:03:15 -0300

    add addTaskHandler, handleChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import styles from './App.module.css';
 import TaskForm from './components/Taskform';
 import Tasklist from './components/Tasklist';
+import { ITask } from './interfaces/Task';
 
 
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer hoje?</h2>
-          <TaskForm btnText='Criar tarefa'/>
+          <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList} />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
