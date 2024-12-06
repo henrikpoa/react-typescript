0336f14f93cc67772447bbbdf2338ce0a40c4acb
henrikpoa <henrik_poa@hotmail.com>
2024-12-06 16:51:27 -0300

    Taskform, Tasklist, bootstrap icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import TaskForm from './components/Taskform';
+import Tasklist from './components/Tasklist';
+
+
 
 function App() {
   return (
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteúdo</h1>
+        <div>
+          <h2>O que você vai fazer hoje?</h2>
+          <TaskForm btnText='Criar tarefa'/>
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <Tasklist />
+          
+        </div>        
       </main>
       <Footer /> 
     </div>
---
+++ src/components/Tasklist.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Tasklist = (props: Props) => {
+  return (
+    <p>Lista de tarefas</p>
+  )
+}
+
+export default Tasklist
