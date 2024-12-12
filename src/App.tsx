b919dfedd9eddd91b54c1443aeb08da55a83a374
henrikpoa <henrik_poa@hotmail.com>
2024-12-12 09:44:45 -0300

    add Modal.tsx, Modal.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import styles from './App.module.css';
 import TaskForm from './components/Taskform';
 import Tasklist from './components/Tasklist';
 import { ITask } from './interfaces/Task';
+import Modal from './components/Modal';
 
 
 
@@ -13,8 +14,19 @@ function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
 
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
   return (
     <div>
+      <Modal 
+      children={<TaskForm btnText='Editar Tarefa' taskList={taskList}/>}
+       />
       <Header />
       <main className={styles.main}>
         <div>
@@ -23,7 +35,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <Tasklist taskList={taskList} />
+          <Tasklist taskList={taskList} handleDelete={deleteTask} />
           
         </div>        
       </main>
--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -5,19 +5,22 @@ import { notEqual } from 'assert'
 
 type Props = {
     taskList: ITask[]
+    handleDelete(id: number): void
 }
 
-const Tasklist = ({taskList}: Props) => {
+const Tasklist = ({taskList, handleDelete}: Props) => {
   return (
     <>
         {taskList.length>0 ? 
             taskList.map((task) =>
             <div key={task.id} className={styles.task}>
-                <h4>{task.title}</h4>
-                <p>Dificuldade: {task.difficulty}</p>
-                <div>
+                <div className={styles.details}>
+                    <h4>{task.title}</h4>
+                    <p>Dificuldade: {task.difficulty}</p>
+                </div>
+                <div className={styles.actions}>
                     <i className='bi bi-pencil'></i>
-                    <i className='bi bi-trash'></i>
+                    <i className='bi bi-trash' onClick={()=>{handleDelete(task.id)}}></i>
                 </div>
             </div>
         ) : <p>Não há tarefas cadastradas</p> }
